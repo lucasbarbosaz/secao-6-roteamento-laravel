2eab70333ef69caff67b14d2530110b3b8696b6d
Lucas Barbosa <corplaxus@gmail.com>
2025-01-20 04:40:54 -0300

    rotas com parametro

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome', [
-    'title' => 'Welcome',
-]);
+Route::get('/users/{id?}/{name?}', function ($id = null, $name = null) {
+    return 'User id: ' . $id . ' name: ' . $name;
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+/*
+ANOTAÇÕES
+
+interrogação no parametro serve para indicar que é opcional
+*/
