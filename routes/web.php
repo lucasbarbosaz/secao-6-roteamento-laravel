3ff4fcc3aad2907ab8fef26f2aefae8f075bc425
Lucas Barbosa <corplaxus@gmail.com>
2025-01-21 02:26:28 -0300

    injetando depedências em rota

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) {
+    dd($request);
+    // return view('welcome');
 });
 
-
-
-Route::fallback(function () {
-    return view('welcome');
-});
 
 /*
 ANOTAÇÕES
